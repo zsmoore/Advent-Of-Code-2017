f109bae3285426a7fc835f6a8ab6d8072ded8874
zsmoore <zsmoore@zachary-moore.com>
2017-12-15 21:53:03 -0500

    day4 done

--- day4.py
+++ day4.py
@@ -5,7 +5,7 @@ def main():
 
     total = 0
     for line in in_file.readlines():
-        if is_valid(line.strip()):
+        if is_valid(line.strip()) and check_anagram(line.strip()):
             total += 1
 
     print(total)
@@ -15,6 +15,21 @@ def is_valid(passphrase):
     if len(set(passphrase.split())) < len(passphrase.split()):
         return False
 
+    return True
+
+def is_anagram(s1, s2):
+    s1 = sorted(s1)
+    s2 = sorted(s2)
+    if s1 == s2:
+        return True
+    return False
+
+def check_anagram(passphrase):
+    passphrase = set(passphrase.split())
+    for word in passphrase:
+        for word2 in passphrase:
+            if word != word2 and is_anagram(word, word2):
+                return False
     return True
 
 if __name__ == "__main__":
